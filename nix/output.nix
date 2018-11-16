4976fbd3fdfea57f0fa8ca5bc96f681608b3efe1
Tom Bereknyei <tom@dds.mil>
2018-11-16 01:12:20 -0500

    init: arbitrary output resources

---
+++ nix/output.nix
@@ -0,0 +1,30 @@
+{ config, lib, name ? "default-output", ... }:
+with lib;
+{
+  options = {
+    name = mkOption {
+      default = "${name}";
+      type = types.str;
+      description = "Name of the output.";
+    };
+    script = mkOption {
+      default = null;
+      type = types.nullOr (types.str);
+      #type = types.nullOr (types.either types.str types.path);
+      description = ''
+        Text of a script which will produce a JSON value.
+        Warning: This uses shell features and is potentially dangerous.
+        Environment variables: 
+            $out is a temp directory available for use.
+        '';
+    };
+    value = mkOption {
+      default = null;
+      type = types.nullOr types.str;
+      description = "Result of running script.";
+    };
+  };
+  config = {
+    _type = "output";
+  };
+}
